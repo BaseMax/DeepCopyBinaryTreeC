069d359c2d2be2c43331b9b4a0915f41349730a4
Max Base <maxbasecode@gmail.com>
2023-02-03 13:04:08 +0000

    Fix bugs

--- DeepCopyBinaryTree.c
+++ DeepCopyBinaryTree.c
@@ -16,11 +16,11 @@
  * 
  * @param data
  * 
- * @return struct node* 
+ * @return struct Node* 
  */
-struct node *newNode(int data)
+struct Node *newNode(int data)
 {
-    struct node *node = (struct node *)malloc(sizeof(struct node));
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
     node->data = data;
     node->left = NULL;
     node->right = NULL;
@@ -32,31 +32,137 @@ struct node *newNode(int data)
  * 
  * @param root
  * 
- * @return struct node* 
+ * @return struct Node* 
  */
-struct node *copyBinaryTreeRecursive(struct node *root)
+struct Node *copyBinaryTreeRecursive(struct Node *root)
 {
     if (root == NULL)
         return NULL;
-    struct node *newRoot = newNode(root->data);
+    struct Node *newRoot = newNode(root->data);
     newRoot->left = copyBinaryTreeRecursive(root->left);
     newRoot->right = copyBinaryTreeRecursive(root->right);
     return newRoot;
 }
 
 /**
+ * @brief Create a Queue object
+ * 
+ * @return struct Queue* 
+ */
+struct Queue* createQueue()
+{
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+    queue->capacity = 10;
+    queue->front = queue->size = 0;
+    queue->rear = queue->capacity - 1;
+    queue->array = (struct Node**) malloc(queue->capacity * sizeof(struct Node*));
+    return queue;
+}
+
+/**
+ * @brief Add an item to the queue
+ * 
+ * @param queue
+ * 
+ * @return struct Node* 
+ */
+void enqueue(struct Queue* queue, struct Node* item)
+{
+    if (queue->size == queue->capacity) return;
+
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size = queue->size + 1;
+}
+
+/**
+ * @brief Delete an item from queue
+ * 
+ * @return struct Node* 
+ */
+struct Node* dequeue(struct Queue* queue)
+{
+    if (isEmpty(queue)) return NULL;
+
+    struct Node* item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size = queue->size - 1;
+    return item;
+}
+
+/**
+ * @brief Check if the queue is empty
+ * 
+ * @param queue 
+ * 
+ * @return int 
+ */
+int isEmpty(struct Queue* queue)
+{
+    return queue->size == 0;
+}
+
+/**
+ * 
  * @brief Copy a binary tree (non-recursively)
  * 
  * @param root
  * 
- * @return struct node* 
+ * @return struct Node* 
  */
-struct node *copyBinaryTree(struct node *root)
+struct Node *copyBinaryTree(struct Node *root)
 {
-    
+    if (root == NULL)
+        return NULL;
+
+    // Create a queue for level order traversal
+    struct Queue *queue = createQueue();
+
+    // Create a new root
+    struct Node *newRoot = newNode(root->data);
+
+    // Enqueue old and new root
+    enqueue(queue, root);
+    enqueue(queue, newRoot);
+
+    // Do level order traversal of the tree
+    while (!isEmpty(queue))
+    {
+        // Dequeue an old node and enqueue its children
+        struct Node *old_node = dequeue(queue);
+        struct Node *new_node = dequeue(queue);
+
+        // Get the old and new node's children
+        struct Node *oldLeft = old_node->left;
+        struct Node *oldRight = old_node->right;
+
+        // If the old node has a left child
+        if (oldLeft)
+        {
+            // Create a new left child for the new node
+            new_node->left = newNode(oldLeft->data);
+
+            // Enqueue the left child
+            enqueue(queue, oldLeft);
+            enqueue(queue, new_node->left);
+        }
+
+        // If the old node has a right child
+        if (oldRight)
+        {
+            // Create a new right child for the new node
+            new_node->right = newNode(oldRight->data);
+
+            // Enqueue the right child
+            enqueue(queue, oldRight);
+            enqueue(queue, new_node->right);
+        }
+    }
+
+    return newRoot;
 }
 
-void printBinaryTree(struct node *root)
+void printBinaryTree(struct Node *root)
 {
     if (root == NULL) return;
 
@@ -69,7 +175,7 @@ void printBinaryTree(struct node *root)
 int main(int argc, char** argv)
 {
     // Create a binary tree
-    struct node *root = newNode(1);
+    struct Node *root = newNode(1);
     root->left = newNode(2);
     root->right = newNode(3);
     root->left->left = newNode(4);
@@ -82,7 +188,7 @@ int main(int argc, char** argv)
     printf("\n");
 
     // Copy the binary tree
-    struct node *newRoot = copyBinaryTree(root);
+    struct Node *newRoot = copyBinaryTree(root);
 
     // Print the copied binary tree
     printf("Copied binary tree: ");
@@ -90,7 +196,7 @@ int main(int argc, char** argv)
     printf("\n");
 
     // Copy the binary tree with recursive algorithm
-    struct node *newRootRecursive = copyBinaryTreeRecursive(root);
+    struct Node *newRootRecursive = copyBinaryTreeRecursive(root);
 
     // Print the copied binary tree with recursive algorithm
     printf("Copied binary tree with recursive algorithm: ");
--- DeepCopyBinaryTree.h
+++ DeepCopyBinaryTree.h
@@ -1,36 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
+/**
+ * @brief A node of a binary tree
+ */
+struct Node
 {
     int data;
-    struct node *left;
-    struct node *right;
+    struct Node *left;
+    struct Node *right;
 };
+
+/**
+ * @brief A queue of nodes
+ */
+struct Queue
+{
+    int front, rear, size;
+    unsigned capacity;
+    struct Node **array;
+};
+
+/**
+ * @brief Create a Queue object
+ * 
+ * @return struct Queue* 
+ */
+struct Queue* createQueue();
+
+/**
+ * @brief Add an item to the queue
+ * 
+ * @param queue
+ * 
+ * @return struct Node* 
+ */
+void enqueue(struct Queue* queue, struct Node* item);
+
+/**
+ * @brief Delete an item from queue
+ * 
+ * @return struct Node* 
+ */
+struct Node* dequeue(struct Queue* queue);
+
+/**
+ * @brief Check if the queue is empty
+ * 
+ * @param queue 
+ * 
+ * @return int 
+ */
+int isEmpty(struct Queue* queue);
 
 /**
  * @brief Create a new node object
  * 
  * @param data
  * 
- * @return struct node* 
+ * @return struct Node* 
  */
-struct node *newNode(int data);
+struct Node *newNode(int data);
 
 /**
  * @brief Copy a binary tree (recursively)
  * 
  * @param root
  * 
- * @return struct node* 
+ * @return struct Node* 
  */
-struct node *copyBinaryTreeRecursive(struct node *root);
+struct Node *copyBinaryTreeRecursive(struct Node *root);
 
 /**
  * @brief Copy a binary tree (non-recursively)
  * 
  * @param root
  * 
- * @return struct node* 
+ * @return struct Node* 
  */
-struct node *copyBinaryTree(struct node *root);
+struct Node *copyBinaryTree(struct Node *root);
